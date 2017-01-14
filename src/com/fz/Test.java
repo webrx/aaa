f04597cc0858567994e0e7159e3f6395cda90766
webrx <webrx@126.com>
2017-01-14 09:19:13 +0800

    Hello world

--- src/com/fz/Test.java
+++ src/com/fz/Test.java
@@ -4,4 +4,7 @@ package com.fz;
  * Created by webrx on 2017/1/14 0014 9:15.
  */
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello-world-2017");
+    }
 }
